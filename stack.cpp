c4c6eb21a36b03b61fb9be1ff979c4309851bde6
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-05-15 15:08:52 +0800

    cetak data dan geser pointer di peek

--- stack.cpp
+++ stack.cpp
@@ -59,4 +59,7 @@ public:
             Node *current = top;
             while (current != NULL)
             {
-                
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            
